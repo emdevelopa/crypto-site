118f41d70616e5ff4982544de72c144e576076a7
emdevelopa <gyimahsunday@gmail.com>
2024-05-15 17:46:41 +0100

    cool

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/home';
+import About from './components/About/about';
+import Navbar from "./components/navBar/navbar";
 
 function App() {
+  
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home route */}
+        <Route path="/about" element={<About />} /> {/* About route */}
+
+        {/* <Route path="*" element={<NotFound />} />{" "} */}
+        {/* Catch-all for unknown routes */}
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,8 @@
+// components/Home.js
+import React from "react";
+
+function Home() {
+  return <h2 style={{color:"red"}}>Home Page</h2>;
+}
+
+export default Home;
