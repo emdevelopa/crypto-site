e95652681846adc513d8952d65fc58dded18118a
emdevelopa <gyimahsunday@gmail.com>
2024-05-19 07:23:46 +0100

    cool

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     fetch("http://localhost:3000");
+  //   }, 1000);
+  // }, []);
+
+  
   return (
     <>
       <div>Navbar</div>
