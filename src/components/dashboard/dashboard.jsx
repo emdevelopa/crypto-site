8971c40b608d894b07651d30f6341b0e2699d4bd
emdevelopa <gyimahsunday@gmail.com>
2024-05-29 13:09:10 +0100

    edited

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -3,7 +3,7 @@ import "./dashboard.css";
 import UserPic from "../../assets/user.jpg";
 
 const Dashboard = () => {
-  const [isPopupVisible, setIsPopupVisible] = useState(flse);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   const togglePopup = () => {
     setIsPopupVisible(!isPopupVisible);
